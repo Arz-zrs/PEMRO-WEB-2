b5f5e37cbf34f15ef0d920d352d279361a208b7b
Arya Arrozza Ridho Syaputra <ozzaboy123@gmail.com>
2026-04-05 19:28:23 +0800

    imported php files from htdocs

---
+++ Modul1/PRAK101.php
@@ -0,0 +1,8 @@
+<?php
+    $nama = "Arya Arrozza Ridho Syaputra";
+    $nim = 2410817210010;
+
+    echo "Hello World in PHP<br>";
+    echo "Nama Praktikan : $nama<br>";
+    echo "NIM : $nim";
+?>
