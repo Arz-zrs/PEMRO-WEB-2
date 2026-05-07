66c1faedb2ea377e86c12978b04de8d1a8863267
Arya Arrozza Ridho Syaputra <ozzaboy123@gmail.com>
2026-05-07 13:48:01 +0800

    revised PRAK302.php

--- Modul3/PRAK302.php
+++ Modul3/PRAK302.php
@@ -1,31 +1,37 @@
 <html>
-<head>
 
-</head>
 <body>
     <form method="post">
-        <label for="height">Tinggi :</label> 
+        <label for="height">Tinggi :</label>
         <input type="number" name="height" id="height"> <br>
-        <label for="image">Alamat Gambar :</label> 
+        <label for="image">Alamat Gambar :</label>
         <input type="text" name="image" id="image"> <br>
         <input type="submit" name="submit" value="Cetak"> <br>
     </form>
     <?php
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $height = $_POST['height'];
-            $image = $_POST['image'];
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $height = $_POST['height'];
+        $image = $_POST['image'];
 
-            while ($height > 0) {
-                for ($j = 0; $j < 2 * (5 - $height); $j++) {
-                    echo "&nbsp;&nbsp; ";
-                }
-                for ($k = 0; $k < $height; $k++) {
-                    echo "<img src='$image' width='20' height='20'> ";
-                }
-                echo "<br>";
-                $height--;
+        $i = 1;
+        while ($i <= $height) {
+            $j = 1;
+            while ($j < $i) {
+                echo "<img src='$image' width='20' height='20' style='visibility:hidden;'>";
+                $j++;
             }
+
+            $k = 1;
+            while ($k <= ($height - $i + 1)) {
+                echo "<img src='$image' width='20' height='20'>";
+                $k++;
+            }
+
+            echo "<br>";
+            $i++;
         }
+    }
     ?>
 </body>
+
 </html>
