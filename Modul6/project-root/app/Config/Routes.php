83e5f8c73fe533215c841f2c88d387f43894cd71
Arya Arrozza Ridho Syaputra <ozzaboy123@gmail.com>
2026-05-25 14:26:51 +0800

    init: base template for codeignite4 web

---
+++ Modul6/project-root/app/Config/Routes.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Controllers\Pages;
+
+$routes->get('/', [Pages::class, 'index']);
+$routes->get('pages', [Pages::class, 'index']);
+$routes->get('(:segment)', [Pages::class, 'view']);
