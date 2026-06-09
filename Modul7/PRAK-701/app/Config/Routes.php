4f3c3b1888f9be6ee4e6afd342e5df7d1bb12581
Arya Arrozza Ridho Syaputra <ozzaboy123@gmail.com>
2026-06-09 08:57:06 +0800

    rouurts

--- Modul7/PRAK-701/app/Config/Routes.php
+++ Modul7/PRAK-701/app/Config/Routes.php
@@ -1,5 +1,11 @@
 <?php
 
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ *  @var RouteCollection $routes
+ */
+
 $routes->get('/', 'AuthController::index');
 $routes->get('/login', 'AuthController::index');
 $routes->post('/login', 'AuthController::login');
